9e1414541ec8c4277ea15c10fb6379878d4b4a30
Jeroen <jpvdwal@gmail.com>
2025-09-05 13:41:53 +0200

    Initial commit

---
+++ src/main/java/com/wallway/ai_chat_poc/AiChatPocApplication.java
@@ -0,0 +1,13 @@
+package com.wallway.ai_chat_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiChatPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiChatPocApplication.class, args);
+	}
+
+}
